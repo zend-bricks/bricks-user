c13c79764864bf9c3cc89fa44b87d8352d23f089
Cyberrebell <stephan.beller@web.de>
2016-09-14 23:57:13 +0200

    added missing sub config require

--- config/module.config.php
+++ config/module.config.php
@@ -8,5 +8,6 @@ return [
     'router' => require 'router.config.php',
     'service_manager' => require 'service_manager.config.php',
     'translator' => require 'translator.config.php',
+    'view_helpers' => require 'view_helpers.config.php',
     'view_manager' => require 'view_manager.config.php',
 ];
